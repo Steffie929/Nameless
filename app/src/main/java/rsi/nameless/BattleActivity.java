014e229a0a9d6d97b85480bbca0e23e251d740a1
Stéphanie <Stephanievangogh@outlook.com>
2016-05-25 18:40:24 +0200

    test

--- app/src/main/java/rsi/nameless/BattleActivity.java
+++ app/src/main/java/rsi/nameless/BattleActivity.java
@@ -26,6 +26,8 @@ public class BattleActivity extends AppCompatActivity {
         this.enemy = battle.getEnemy();
         playerTurn = false;
 
+        //More code!
+
         HP_player = (TextView) findViewById(R.id.hpbar_player);
         HP_enemy = (TextView) findViewById(R.id.hpbar_enemy);
         playerName = (TextView) findViewById(R.id.name_player);
