9071b07e05930f09e239beca8cfd44aa89117c22
SCRK16 <rick.koenders@hotmail.com>
2016-05-30 17:27:12 +0200

    BackpackActivity toegevoegd
    Doet nog niets en is ook nog nergens te bereiken

---
+++ app/src/main/java/rsi/nameless/BackpackActivity.java
@@ -0,0 +1,13 @@
+package rsi.nameless;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BackpackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backpack);
+    }
+}
